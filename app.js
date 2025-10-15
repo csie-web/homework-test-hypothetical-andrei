f78cf17c038132a8e0c1de73a0fabb55037d972a
AT <andrei.toma@ie.ase.ro>
2025-10-15 08:30:51 +0300

    a

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 function render(input, values){
-  
+  let result = ''
+  if (typeof input !== 'object' || typeof values !== 'object') {
+      throw new Error('InvalidType')
+  }
+  for (const key in input) {
+      if (typeof input[key] === 'string') {
+          let rendered = input[key]
+          for (const valueKey in values) {
+              rendered = rendered.replace('${' + valueKey + '}', values[valueKey])
+          }
+          console.log(values)
+          console.log(`rendering string for ${key} is ${rendered}`)
+          result += `<${key}>${rendered}</${key}>`
+      } else {
+          if (typeof input[key] === 'object') {
+              result += `<${key}>${render(input[key], values)}</${key}>`
+          }
+      }
+  }
+  return result
 }
 
 module.exports = {
